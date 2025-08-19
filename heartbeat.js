5650e0e5699d08956cd431171420fb25d241abfe
notadose <notadose@proton.me>
2025-08-18 21:32:53 -0400

    provide nothing of value

--- heartbeat.js
+++ heartbeat.js
@@ -1,3 +1,16 @@
+/*
+
+15BPM is dangerous,
+you should see a doctor
+
+isn't it wonderful that most people will go their entire lives with a heart that never stops beating?
+the body is cruel and beautiful
+
+strange that one part of the brain would so lead so loyal an organ
+while another would shed tears at its rhythm
+
+*/
+
 let cyclePeriod = 4000;
 let cycleTime = 0;
 let lastTime = null;
